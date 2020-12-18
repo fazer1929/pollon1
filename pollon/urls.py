a12b2eb5142628c772024796ba77869ff0a40bc3
fazer1929 <abhishekagrawal8888@gmail.com>
2020-12-18 21:02:38 +0530

    ready for deployment V0.1

--- pollon/urls.py
+++ pollon/urls.py
@@ -18,11 +18,10 @@ from django.urls import path,include
 from . import settings
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-        url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+    path('admin/', admin.site.urls), 
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
